73c8a6ef1f8294387ddbfad6bb08ba3451818ee3
Franklin Lorran <lorranfranklin.dev@gmail.com>
2024-06-30 20:58:26 -0300

    criando o arquivo de teste para ir testando os trabalhos e após isso incrementar mudanças ao arquivo principal.

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ menuBtn.addEventListener("click", () => {
 closeBtn.addEventListener("click", () => {
   menu.classList.remove("active");
 });
+
+window.addEventListener("scroll", function () {
+  const header = this.document.querySelector("header");
+  header.classList.toggle("sticky", this.window.scrollY > 0);
+});
+
